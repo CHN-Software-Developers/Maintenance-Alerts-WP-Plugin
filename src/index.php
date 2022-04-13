0ebeaed3c69a6dae57d2a0863f01683203694cbe
Himashana <himashanasuraweera5@gmail.com>
2022-04-13 15:16:13 +0530

    Changes: 	deleted:    src/about.php 	modified:   src/index.php

--- src/index.php
+++ src/index.php
@@ -86,26 +86,36 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
  //Display Maintenance Alert
  add_action("wp_body_open", "add_alert");
  
- function add_alert(){
-	 //Setup maintenance mode
-	 if(get_option('Maintenance_mode_action') == "enable --maintenance"){
-		include_once dirname( __FILE__ ) . '\maintenance_mode.php';
-	}
+ function add_alert(){	 
+	 //If the user select 'Enabled', a maintenance alert/mode will display on the website.
+	 $is_show_maintenance = true;
 	 
-		 
-	 //If the user select 'Enabled', a maintenance alert will display on the top of the website.
-	 if(get_option('Action_select') == "Enabled"){
-		 //If the header text is empty, it will show the default text.
-		 if(get_option('field') <> ""){
-			?> <a href="<?php echo get_option('Onclick_event'); ?>" style="text-decoration:none;"><div style="color:<?php echo get_option('textcolor'); ?>; background-color:<?php echo get_option('backgroundcolor'); ?>; padding:<?php echo get_option('alertPadding'); ?>px;"><center><h4 style="font-size:<?php echo get_option('fontSize'); ?>px; color:<?php echo get_option('textcolor'); ?>"> <?php echo get_option('field'); ?> </h4></center></div></a> <?php
-		 }else{
-			?> <a href="<?php echo get_option('Onclick_event'); ?>" style="text-decoration:none;"><div style="color:<?php echo get_option('textcolor'); ?>; background-color:<?php echo get_option('backgroundcolor'); ?>; padding:<?php echo get_option('alertPadding'); ?>px;"><center><h4 style="font-size:<?php echo get_option('fontSize'); ?>px; color:<?php echo get_option('textcolor'); ?>;"> <?php echo'This site will be unavailable on Wednesday, 26 January 2022 from 6.30AM to 12.00PM due to planned maintenance.'; ?> </h4></center></div></a> <?php
-		 }
+	 //Force disable maintenance alert/mode when user logged in
+	 if(get_option('Force_maintenance_when_loggedin') == "Yes"){
+		if(is_user_logged_in()){
+			$is_show_maintenance = false;
+		}
+	 }
+
+	 //Display only of user enabled and $is_show_maintenance variable set to true.
+	 if(get_option('Action_select') == "Enabled" && $is_show_maintenance == true){
+		//Display maintenance mode.
+		if(get_option('Maintenance_type') == "Maintenance_Mode"){
+				include_once dirname( __FILE__ ) . '\maintenance_mode.php';	
+		}else{ //Display maintenance alert.
+			//If the header text is empty, it will show the default text.
+			if(get_option('field') <> ""){
+				?> <a href="<?php echo get_option('Onclick_event'); ?>" style="text-decoration:none;"><div style="color:<?php echo get_option('textcolor'); ?>; background-color:<?php echo get_option('backgroundcolor'); ?>; padding:<?php echo get_option('alertPadding'); ?>px;"><center><h4 style="font-size:<?php echo get_option('fontSize'); ?>px; color:<?php echo get_option('textcolor'); ?>"> <?php echo get_option('field'); ?> </h4></center></div></a> <?php
+			}else{
+				?> <a href="<?php echo get_option('Onclick_event'); ?>" style="text-decoration:none;"><div style="color:<?php echo get_option('textcolor'); ?>; background-color:<?php echo get_option('backgroundcolor'); ?>; padding:<?php echo get_option('alertPadding'); ?>px;"><center><h4 style="font-size:<?php echo get_option('fontSize'); ?>px; color:<?php echo get_option('textcolor'); ?>;"> <?php echo'This site will be unavailable on Wednesday, 26 January 2022 from 6.30AM to 12.00PM due to planned maintenance.'; ?> </h4></center></div></a> <?php
+			}
+		}
 	 }
 	 
  }
  
  function alert_register_settings(){
+	 register_setting('Maintenance_type_group', 'Maintenance_type');
 	 register_setting('option_group', 'field');
 	 register_setting('option_group', 'Action_select');
 	 register_setting('option_group', 'textcolor');
@@ -113,6 +123,7 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
 	 register_setting('option_group', 'fontSize');
 	 register_setting('option_group', 'alertPadding');
 	 register_setting('option_group', 'Onclick_event');
+	 register_setting('option_group', 'Force_maintenance_when_loggedin');
 	 register_setting('option_group', 'Maintenance_mode_action');
 	 register_setting('Activation_option_group', 'pro_activate');
 	 register_setting('configure_advanced_settings', 'configuration_id');
@@ -131,23 +142,30 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
  	 $configuration_id_old_version = "config_0001"; // old version id
  	 $configuration_id_new_version = "config_0002"; // new version id
  	 $configuration_success_id = "config_0002_done";// configuration success identification id
-	 
-	 echo'<h1>Maintenance Alerts</h1><br>';
 	 ?>
+	
+	<h1>Maintenance Alerts</h1><br>
+	<?php settings_errors(); ?>
 	 <!--Free Product activation-->
 	 <div class="wrap top-bar-wrapper" style="background-color:white; padding:10px;">
+	 
 		 <?php
 			 if(get_option('pro_activate') <> ""){
-				 echo'<h3>Product activated</h3>';
+				?>
+					<div class="notice notice-success is-dismissible">
+						<h3>Product activated</h3>
+					</div>
+				<?php
 			 }else{
 				 ?>
+				 <div class="notice notice-warning is-dismissible">
 				 <form method="post" action="options.php">
-					 <?php settings_errors(); ?>
 					 <?php settings_fields('Activation_option_group'); ?>
 					 <h3>Product activation</h3>
 					 <input type="email" name="pro_activate" value="<?php echo get_option('admin_email') ?>" placeholder="Enter your Email" style="width:30%;" disabled>
 				 	<?php submit_button('Activate plugin'); ?>
 		 		</form>
+				</div>
 			<?php	 
 			 }
 		    ?>
@@ -155,11 +173,30 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
 	  
 	 <!--Maintenance Alerts settings box-->
 	 <div class="wrap top-bar-wrapper" style="background-color:white; padding:10px;">
-		 <!-- General settings -->
-		 <h3>General settings</h3><hr><br>
+		 <div style="background-color:#E2E7EC; padding-left:20px; padding-top:1px; padding-bottom:30px;">
+		 
+		 <!-- toggle maintenance display type. -->
 		 <form method="post" action="options.php">
-			 <?php settings_errors(); ?>
+			<?php settings_fields('Maintenance_type_group'); ?>
+			<?php
+				if(get_option('Maintenance_type') == "Maintenance_Mode"){
+					submit_button('Switch to maintenance alert');
+					?>
+						<input type="text" value="Maintenance_Alert" style="display:none;" name="Maintenance_type" placeholder="none">
+					<?php
+				}else{
+					submit_button('Switch to maintenance mode');
+					?>
+						<input type="text" value="Maintenance_Mode" style="display:none;" name="Maintenance_type" placeholder="none">
+					<?php
+				}
+			?>
+		</form>
+
+
+		 <form method="post" action="options.php">
 			 <?php settings_fields('option_group'); ?>
+			 <!-- Enable and disable maintenance mode/alert-->
 			 Action : 
 			 <?php
 				 if(get_option('Action_select') == "Enabled"){
@@ -178,7 +215,13 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
 			  		<?php 
 				 }
 				 ?>
-			 <br><br>
+			</div>
+			<!-- General settings for alert -->
+			<?php
+				if(get_option('Maintenance_type') == "Maintenance_Alert"){
+			?>
+			<h3>General settings</h3><hr>	 
+			 <br>
 			 <label for="alert_field_txt">Maintenance alert text :</label><br>
 			 <input type="text" name="field" class="large-text" value="<?php 
 				 //If the input field is empty, it will save the default text.
@@ -189,12 +232,22 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
 				 }
 				 
 				 ?>">
-				 
+			<?php
+				}
+			?>	 
 				 <!-- The configuration_id change from version to version. -->
 				 <?php if(get_option('configuration_id') == $configuration_id_new_version){ ?>
-					 <!-- Advance settings -->
-					 <br><br><h3>Advanced settings</h3><hr><br>
-					 
+	
+					 <!-- Advance settings for alert/ General settings for maintenance mode -->
+					 <?php
+						if(get_option('Maintenance_type') == "Maintenance_Alert"){
+							echo'<br><br><h3>Advanced settings</h3><hr><br>';
+						}else{
+							echo'<br><br><h3>General settings</h3><hr><br>';
+						}
+			 		 ?>	
+					  
+					  
 					 <!-- Change text color -->
 					 <label for="textcolor">Text color : </label>
 					 <input type="text" id="textcolor" name="textcolor" value="<?php
@@ -222,7 +275,7 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
 					 <br><br>
 					 
 					 <!-- Change font size -->
-					 <label for="fontSize">Font size : <label style="background-color:blue; color:white; font-weight: bold;">&nbsp;New&nbsp;</label></label>
+					 <label for="fontSize">Font size :</label>
 					 <input type="text" id="fontSize" name="fontSize" value="<?php
 						 if(get_option('fontSize') <> ""){
 						 	echo get_option('fontSize'); 
@@ -236,7 +289,7 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
 					 <br><br>
 					 
 					 <!-- Change padding -->
-					 <label for="alertPadding">Padding : <label style="background-color:blue; color:white; font-weight: bold;">&nbsp;New&nbsp;</label></label>
+					 <label for="alertPadding">Padding :</label>
 					 <input type="text" id="alertPadding" name="alertPadding" value="<?php
 						 if(get_option('alertPadding') <> ""){
 						 	echo get_option('alertPadding'); 
@@ -253,6 +306,27 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
 					 <label for="Onclick_event">On click :</label>
 					 <input type="text" id="Onclick_event" name="Onclick_event" value="<?php echo get_option('Onclick_event'); ?>" placeholder="https://">
 					 
+					 <!-- Selection to force disable when user logged in  -->
+					 <br><br><label for="Onclick_event">Force disable when user logged in <label style="background-color:blue; color:white; font-weight: bold;">&nbsp;New&nbsp;</label> : </label>
+					 <?php
+						if(get_option('Force_maintenance_when_loggedin') == "Yes" || get_option('Force_maintenance_when_loggedin') == ""){
+							?>
+							<select id="Force_maintenance_when_loggedin" name="Force_maintenance_when_loggedin">
+							  <option value="Yes">Yes</option>
+							  <option value="No">No</option>
+							 </select>
+							  <?php
+						 }else{
+							?>
+							<select id="Force_maintenance_when_loggedin" name="Force_maintenance_when_loggedin">
+							  <option value="No">No</option>
+							  <option value="Yes">Yes</option>
+							 </select>
+							  <?php 
+						 }
+					 ?>
+
+
 					 <!-- Maintenance mode -->
 					 <br><br><h3>Maintenance mode</h3><hr><br>
 					 
@@ -313,5 +387,29 @@ add_filter('template_include', 'maintenance_alert_template_select', 99);
  
  //About the plugin
   function MaintenanceAlertsMenuAbout(){
-	include_once dirname( __FILE__ ) . '\about.php';
+	echo'<h1>Maintenance Alerts - About</h1>';
+    ?>
+         <div class="wrap top-bar-wrapper" style="background-color:white; padding:10px;">
+            * Plugin Name:       Maintenance alerts
+            <br>
+            * Plugin URI:        https://chnsoftwaredevelopers.com/Himashana/WP-Plugins/Maintenance_alerts
+            <br>
+            * Description:       This plugin shows the website maintenance scheduled information to the visitors on the top of the website.
+            <br>
+            * Version:           1.2.0
+            <br>
+            * Requires at least: 5.2
+            <br>
+            * Requires PHP:      7.2
+            <br>
+            * Author:            Himashana
+            <br>
+            * Author URI:        https://chnsoftwaredevelopers.com/
+            <br>
+            * License:           GPL v2 or later
+            <br>
+            * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+       </div>
+       
+    <?php
  }
